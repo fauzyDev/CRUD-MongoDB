ec9368031259d63fc0968fbfcccf0c8a4000ed07
fauzyDev <haliffauzy45@gmail.com>
2024-12-31 13:55:14 +0700

    fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,17 @@
 import express from "express"
 import { response } from "../res/costumResponse.js"
-import { getUser, createUser, updateUser } from "../controller/controller.js"
+import { getUser, createUser, updateUser, deleteUser } from "../controller/controller.js"
 
 export const router = express.Router()
-router.get('/api/cek', async (req, res) => {
-    response(200, { data: true }, "cek", res)
+router.get('/api', getUser, async (req, res) => {
+    response(200, { data: "sucess" }, "Done", res)
 })
-
-router.get('/api', getUser);
-router.post('/api', createUser);
-router.put('/api', updateUser)
+router.post('/api', createUser, async (req, res) => {
+    response(201, { data: "create sucess" }, "Done", res)
+});
+router.put('/api', updateUser, async (req, res) => {
+    response(201, { data: "update sucess" }, "Done", res)
+});
+router.delete('/api', deleteUser, async (req, res) => {
+    response(200, { data: "delete sucess" }, "Done", res)
+});
